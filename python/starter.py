00880840a5ea4ffcd1a87d561168559b994d0e65
bikenthusiast <tobias.kapfhammer@yahoo.de>
2020-05-26 10:40:10 +0200

    proof that env works

--- python/starter.py
+++ python/starter.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-version https://git-lfs.github.com/spec/v1
-oid sha256:47505165152d78becee14a9cc127464d3e41d03ea470034a26a1b5d4e2bcde0d
-size 54
-=======
 import tensorflow
+import keras
+
 print ("Hello World!")
->>>>>>> 70684b378f12a06694ff66a6e34432c9140387c8
