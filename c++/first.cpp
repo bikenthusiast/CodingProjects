07c68eef4e19a6fc5ea1a49b287814b234354975
bikenthusiast <tobias.kapfhammer@yahoo.de>
2020-06-17 10:00:52 +0200

    Added Visual Studio Code launch.json and tasks.json file and added simple code files

---
+++ c++/first.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+
+int main()
+{
+    int age = 10;
+    string name = "Tobias";
+
+    cout << "Name is " << name << endl;
+    cout << "Age is :" << age << endl;
+    return 0;
+}
