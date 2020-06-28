b3bf67e3c57fb84b67c70ed96352ef4f378f0a80
bikenthusiast <tobias.kapfhammer@yahoo.de>
2020-06-28 14:01:22 +0200

    Added openvslam to the repo via docker and catkin_ws

---
+++ slam_catkin_ws/build/openvslam/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/parallels/Coding/CodingProjects/slam_catkin_ws/devel/.private/openvslam:$CMAKE_PREFIX_PATH"
+export PWD="/home/parallels/Coding/CodingProjects/slam_catkin_ws/build/openvslam"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/parallels/Coding/CodingProjects/slam_catkin_ws/devel/.private/openvslam/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/parallels/Coding/CodingProjects/slam_catkin_ws/src/openvslam/ros/src/openvslam:$ROS_PACKAGE_PATH"
---
+++ slam_catkin_ws/devel/share/image_geometry/cmake/image_geometryConfig.cmake
@@ -0,0 +1,1 @@
+/home/parallels/Coding/CodingProjects/slam_catkin_ws/devel/.private/image_geometry/share/image_geometry/cmake/image_geometryConfig.cmake
