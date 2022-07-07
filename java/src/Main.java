99dc0204f636ae0d073ff892aa06e2edf2b99627
Tobi <Tobias.Kapfhammer@yahoo.de>
2022-07-07 16:57:46 +0200

    Added JAVA files

---
+++ java/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Tobias";
+        String job = "Playboy";
+        int hour = 15;
+        int alter = 30;
+        if (hour < 12) {
+            System.out.println("Guten Morgen!");
+        } else {
+            System.out.println("Guten Abend!");
+        }
+        System.out.println("Ich heiße " + name + ". Ich bin ein " + job + "! \n Ich bin " + alter + " Jahre alt.");
+    }
+    }
